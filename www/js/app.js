26181936cebefd72806b6f1dae55fe55d184fb93
Adrian Estrada <edsadr@gmail.com>
2015-01-20 00:20:31 -0500

    Starting with Famous and Angular, a demo project integrated and bower init

---
+++ www/js/app.js
@@ -0,0 +1,11 @@
+angular.module('MyPhotos',['famous.angular', 'ui.router'])
+.config(function($stateProvider, $urlRouterProvider){
+
+    $urlRouterProvider.otherwise("/");
+    $stateProvider
+    .state("main", {
+        url: "/",
+        templateUrl: "views/main.html",
+        controller: "MainCtrl"
+    });
+});
